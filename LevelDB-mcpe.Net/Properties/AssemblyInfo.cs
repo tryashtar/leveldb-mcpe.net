279005e4d2d876ceed12c257a6cf2388acda3ea0
Daan Verstraten <Huntondoom@gmail.com>
2020-03-14 17:24:33 +0100

    Updated

--- LevelDB-mcpe.Net/Properties/AssemblyInfo.cs
+++ LevelDB-mcpe.Net/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LevelDB-mcpe.Net")]
+[assembly: AssemblyTitle("LevelDB-MCPE.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LevelDB-mcpe.Net")]
+[assembly: AssemblyProduct("LevelDB-MCPE.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("887fecd2-b6a7-44bc-a986-0e6518b9c3d6")]
+[assembly: Guid("c9fedfaf-baa8-4112-8743-66994164af73")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Leveldb-Mcpe/port/port.h
+++ Leveldb-Mcpe/port/port.h
@@ -11,11 +11,11 @@
 // porting to a new platform, see "port_example.h" for documentation
 // of what the new port_<platform>.h file must provide.
 #if defined(LEVELDB_PLATFORM_POSIX) || defined(LEVELDB_PLATFORM_ANDROID)
-#  include "Leveldb-Mcpe/port/port_posix.h"
+#include "port/port_posix.h"
 #elif defined(LEVELDB_PLATFORM_CHROMIUM)
-#  include "Leveldb-Mcpe/port/port_chromium.h"
+#include "port/port_chromium.h"
 #elif defined(WIN32)
-#  include "Leveldb-Mcpe/port/port_win.h"
+#include "port/port_win.h"
 #endif
 
 #endif  // STORAGE_LEVELDB_PORT_PORT_H_
