bf998de66498d620475bb225bbc198b1ec6a85cb
Daan Verstraten <Huntondoom@gmail.com>
2020-03-14 15:37:53 +0100

    Updated

--- Leveldb-Mcpe/util/win_logger.cc
+++ Leveldb-Mcpe/util/win_logger.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#include "Leveldb-Mcpe/util/win_logger.h"
+#include "util/win_logger.h"
 
 #ifdef WIN32
 
---
+++ Test/Test.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test.Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test.Net")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83d6b83d-b7a7-4e36-9c25-6ca12ce0d95a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
