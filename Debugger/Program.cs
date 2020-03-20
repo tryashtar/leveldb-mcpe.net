6b5c735bba9d837a58cf4941510b38fc995a071c
Daan Verstraten <Huntondoom@gmail.com>
2020-03-20 13:19:46 +0100

    This versions works!

--- Debugger/Program.cs
+++ Debugger/Program.cs
@@ -4,7 +4,13 @@ using LevelDB;
 namespace Debugger {
     internal class Program {
         private static void Main(String[] args) {
-            Options O = new Options();
+            Options O = new Options {
+                CreateIfMissing = true
+            };
+
+            DB Values = new DB("test", O);
+
+            Console.ReadLine();
         }
     }
 }
--- LevelDB-mcpe.Net/Static Classes/LevelDBInterop - Initialize.cs
+++ LevelDB-mcpe.Net/Static Classes/LevelDBInterop - Initialize.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace LevelDB {
     public static partial class LevelDBInterop {
@@ -14,9 +15,16 @@ namespace LevelDB {
             String folder = Path.GetDirectoryName(assembly.Location);
             String path = Path.Combine(folder, "LevelDB-MCPE.dll");
 
-            IntPtr h = LoadLibrary(path);
-            if (h == IntPtr.Zero) {
-                throw new ApplicationException("Cannot load LevelDB-MCPE.dll");
+            if (File.Exists(path)) {
+                IntPtr h = LoadLibrary(path);
+
+                if (h == IntPtr.Zero) {
+                    Int32 Value = Marshal.GetLastWin32Error();
+                    throw new ApplicationException($"Cannot load LevelDB-MCPE.dll, Error Code: {Value}");
+                }
+            }
+            else {
+                throw new ApplicationException("Cannot find LevelDB-MCPE.dll");
             }
         }
     }
