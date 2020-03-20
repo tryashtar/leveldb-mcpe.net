0e9341fcc22f750aa435f8317d4f8777cc7827dc
Daan Verstraten <Huntondoom@gmail.com>
2020-03-20 14:26:53 +0100

    uPDATED

--- LevelDB-mcpe.Net/Static Classes/LevelDBInterop - Initialize.cs
+++ LevelDB-mcpe.Net/Static Classes/LevelDBInterop - Initialize.cs
@@ -15,17 +15,34 @@ namespace LevelDB {
             String folder = Path.GetDirectoryName(assembly.Location);
             String path = Path.Combine(folder, "LevelDB-MCPE.dll");
 
-            if (File.Exists(path)) {
-                IntPtr h = LoadLibrary(path);
-
-                if (h == IntPtr.Zero) {
-                    Int32 Value = Marshal.GetLastWin32Error();
-                    throw new ApplicationException($"Cannot load LevelDB-MCPE.dll, Error Code: {Value}");
+            if (!File.Exists(path)) {
+                if (Environment.Is64BitProcess) {
+                    Copy(Path.Combine(folder, "LevelDB-MCPE-x64.dll"), path);
+                }
+                else {
+                    Copy(Path.Combine(folder, "LevelDB-MCPE-x86.dll"), path);
                 }
             }
-            else {
-                throw new ApplicationException("Cannot find LevelDB-MCPE.dll");
+
+            IntPtr h = LoadLibrary(path);
+
+            if (h == IntPtr.Zero) {
+                Int32 Value = Marshal.GetLastWin32Error();
+                throw new ApplicationException($"Cannot load LevelDB-MCPE.dll, Error Code: {Value}");
             }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        private static void Copy(String source, String destination) {
+            if (!File.Exists(source)) {
+                throw new ApplicationException($"Cannot find: {source}");
+            }
+
+            File.Copy(source, destination, true);
         }
     }
 }
