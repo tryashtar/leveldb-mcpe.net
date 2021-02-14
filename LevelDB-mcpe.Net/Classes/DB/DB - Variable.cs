d8520d7a0ebf678b4e369b449aa76390763d348d
DaanV2 <daanverstraten@hotmail.com>
2021-02-14 08:23:01 +0100

    auto: Code cleanup

--- LevelDB-mcpe.Net/Classes/DB/DB - Variable.cs
+++ LevelDB-mcpe.Net/Classes/DB/DB - Variable.cs
@@ -3,7 +3,7 @@
     /// A DB is a persistent ordered map from keys to values.
     /// A DB is safe for concurrent access from multiple threads without any external synchronization.
     /// </summary>
-    public partial class DB  {
+    public partial class DB {
         private Options _Options;
     }
 }
