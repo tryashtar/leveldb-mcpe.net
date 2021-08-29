46b13eb872601adae7f46047f8edeb1937feef41
DaanV2 <daanverstraten@hotmail.com>
2021-08-29 15:25:57 +0200

    Removed duplicate

--- LevelDB-mcpe.Net 5.0/Classes/DB/DB - Variable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LevelDB {
-    /// <summary>
-    /// A DB is a persistent ordered map from keys to values.
-    /// A DB is safe for concurrent access from multiple threads without any external synchronization.
-    /// </summary>
-    public partial class DB  {
-        private Options _Options;
-    }
-}
